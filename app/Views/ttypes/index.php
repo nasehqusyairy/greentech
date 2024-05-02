c05d2d533cf07a3f8123bbb8dd222d5f53eeaa8b
Nusantara One <nasyikhqusyairy@gmail.com>
2024-05-02 16:06:10 +0700

    modified:   app/Views/ttypes/index.php

--- app/Views/ttypes/index.php
+++ app/Views/ttypes/index.php
@@ -16,7 +16,7 @@ if (!empty($message)) : ?>
 <div class="card">
   <div class="card-body">
     <div class="mb-3">
-      <a href="/ttypes/create" class="btn btn-primary"><i class="bi bi-plus"></i>New ttype</a>
+      <a href="/ttypes/create" class="btn btn-primary"><i class="bi bi-plus"></i>New Type</a>
     </div>
     <ul class="nav nav-tabs" id="tab">
       <li class="nav-item">
