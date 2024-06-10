86f94fbdfccb8dc00f630b21c6727f786f973893
Nusantara One <nasyikhqusyairy@gmail.com>
2024-06-10 18:33:13 +0700

    modified:   app/Controllers/Users.php 	modified:   app/Helpers/email_helper.php

--- app/Helpers/email_helper.php
+++ app/Helpers/email_helper.php
@@ -11,12 +11,15 @@ function set_mail($title, $message, $redirect = null, $button = null): array
 
 // "Hello, <strong>$name</strong>! Thanks for joining us! We're excited to have you as part of our community. To get started, please activate your account by clicking the button below."
 
-function send_email(array $mail, $address): bool
+function send_email(array $mail, $address)
 {
   $email = \Config\Services::email();
   $email->setFrom(getenv('SMTP_USER'), getenv('SMTP_NAME'));
   $email->setTo($address);
   $email->setSubject($mail['title']);
   $email->setMessage(view('email/index', $mail));
-  return $email->send();
+  $email->send();
+
+  echo $email->printDebugger();
+  die;
 }
