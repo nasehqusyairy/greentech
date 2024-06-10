44f5ab37fb430752efd228e8895e0ab153f7c87f
Nusantara One <nasyikhqusyairy@gmail.com>
2024-06-10 23:20:01 +0700

    modified:   app/Views/auth/index.php 	new file:   app/Views/auth/index2.php

--- app/Views/auth/index.php
+++ app/Views/auth/index.php
@@ -3,59 +3,81 @@
 /**
  * @var CodeIgniter\View\View $this
  */
-$this->extend('auth/layout');
-$this->section('content');
 ?>
-<div class="row justify-content-center">
-  <div class="col-md-6">
-    <div class="card my-3">
-      <div class="card-body">
-        <form action="<?= base_url('auth/login'); ?>" method="post">
-          <?= csrf_field(); ?>
-          <h3 class="text-center">Welcome Back!</h3>
-          <?php if (session()->has('errors')) : ?>
-            <div class="alert alert-danger" role="alert">
-              <?php
-              if (count(session('errors')) > 1) :
-              ?>
-                <ul>
-                  <?php foreach (session('errors') as $error) : ?>
-                    <li><?= $error ?></li>
-                  <?php endforeach; ?>
-                </ul>
-              <?php else : ?>
-                <?= array_values(session('errors'))[0] ?>
-              <?php endif; ?>
-            </div>
-          <?php endif; ?>
-          <?php if (session()->has('message')) : ?>
-            <div class="alert alert-success" role="alert">
-              <?= session('message') ?>
-            </div>
-          <?php endif; ?>
-          <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" value="<?= old('email'); ?>" required>
-          </div>
-          <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password" value="<?= old('password'); ?>" required>
-          </div>
-          <p>
-            <a href="<?= base_url('auth/forgot'); ?>">Forgot Password?</a>
-          </p>
-          <div class="d-grid gap-2 mb-3">
-            <button type="submit" class="btn btn-primary">Login</button>
-            <a href="<?= base_url('auth/register'); ?>" class="btn btn-secondary">Sign Up</a>
-          </div>
-          <p class="text-center">Or</p>
-          <a href="<?= base_url('auth/google'); ?>" class="btn btn-outline-primary w-100 mb-3"><i class="bi bi-google me-3"></i>
-            Continue
-            with
-            Google</a>
-        </form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Green Tech | Sign In</title>
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/main.min.css">
+  <link rel="shortcut icon" href="<?= base_url(); ?>assets/img/LOGO BULAT.png">
+
+  <!-- Theme Color -->
+  <meta name="theme-color" content="#008080">
+  <!-- <meta name="theme-color" content="#0d6efd"> -->
+  <style>
+    .bd-login-form {
+      min-height: 100vh;
+      flex: 4;
+      display: flex;
+      align-items: center;
+      background-position: center;
+      background-size: cover;
+    }
+
+    .bd-login-cover {
+      min-height: 100vh;
+      flex: 3;
+      background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/assets/img/bg3.jpg);
+      background-size: cover;
+      background-position: center;
+    }
+
+    .alert ul {
+      margin: 0;
+    }
+  </style>
+</head>
+
+<body style="background-color: var(--bs-gray-900);">
+  <div class="bd-login-layout bg-light d-block d-lg-flex">
+
+    <section class="bd-login-cover p-3 p-5 pb-0 d-flex flex-column justify-content-between">
+      <!-- tampilkan logo dan deskripsi aplikasi -->
+      <div class="text-center">
+        <img src="<?= base_url(); ?>assets/img/LOGO_TRANSPARAN.png" alt="Logo" height="100">
       </div>
-    </div>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-6">
+            <div class="text-white text-center">
+              <h1>THE 14TH INTERNATIONAL CONFERENCE ON GREEN TECHNOLOGY (ICGT)</h1>
+              <p>ICGT is an annual multidisciplinary meeting for cultivating and promoting
+                interactions between educators, scientists, engineers, and industry researchers in studying the development
+                of science & technology and related area. </p>
+              <p class="text-center">
+                <a href="<?= base_url(); ?>" class="text-white">Read more</a>
+              </p>
+              <p><a href="<?= base_url('auth/google'); ?>" class="btn btn-primary btn-lg"><i class="bi bi-google me-3"></i>
+                  Continue
+                  with
+                  Google</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p class="text-white text-center">© <?= date('Y'); ?>. All rights reserved.</p>
+      </div>
+    </section>
+
   </div>
-</div>
-<?php $this->endSection(); ?>
+
+  <!-- JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
